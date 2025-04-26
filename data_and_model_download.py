9ebff1c5bdfaf4ee1960a0c823c3f50769b961b9
Agada Joseph <joe88data@gmail.com>
2025-04-26 16:30:57 -0400

    Update data_and_model_download.py

--- data_and_model_download.py
+++ data_and_model_download.py
@@ -20,7 +20,7 @@ from transformers import (
 )
 
 # ── CONFIG ───────────────────────────────────────────────────────────────────
-MODEL_ID="meta-llama/Llama-2-7b"
+MODEL_ID="mistralai/Mistral-7B-Instruct-v0.1"
 DATA_NAME   = "glue"
 DATA_CONFIG = "sst2"
 OUT_ROOT    = Path("finetune_data")
